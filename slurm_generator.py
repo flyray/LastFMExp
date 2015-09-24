5d4a6450816d9818617a2b531fd6fae553b055cd
Huazheng Wang <eagermindwhz@gmail.com>
2015-09-24 10:11:41 -0400

    Small fix.

--- slurm_generator.py
+++ slurm_generator.py
@@ -1,7 +1,7 @@
 dataset_arr = [['LastFM', './Dataset/hetrec2011-lastfm-2k/user_relation_adjacency_list.dat.part.'],['Delicious', './Dataset/hetrec2011-delicious-2k/user_relation_adjacency_list.dat.part.']]
 partition_arr = [['parallel','16','48:00:00','OMP_NUM_THREADS=16'], ['serial','1', '120:00:00', 'OMP_NUM_THREADS=8']]
 clusternum_arr = ['50', '100', '200']
-alg_arr = ['LinUCB', 'M_LinUCB', 'Uniform_LinUCB', 'CoLin', 'GOBLin']
+alg_arr = ['LinUCB', 'M_LinUCB', 'Uniform_LinUCB', 'CoLinUCB', 'GOBLin']
 diagnol_arr = ['Opt', 'Max', '0.3' 'Org']
 
 s = [
@@ -25,7 +25,7 @@ for dataset in dataset_arr:
 	for num in clusternum_arr:
 		for alg in alg_arr[-2:]:
 			t = s[:]
-			filename = partition[0]+'_'+dataset[0]+'_'+num+'_'+alg+diagnol
+			filename = partition[0]+'_'+dataset[0]+'_'+num+'_'+alg+'_'+diagnol
 			t[2] += partition[1]
 			t[3] += partition[2]
 			t[4] += partition[0]
@@ -43,7 +43,7 @@ for dataset in dataset_arr:
 	for num in clusternum_arr:
 		for alg in alg_arr[:-2]:
 			t = s[:]
-			filename = partition[0]+'_'+dataset[0]+'_'+num+'_'+alg+diagnol
+			filename = partition[0]+'_'+dataset[0]+'_'+num+'_'+alg+'_'+diagnol
 			t[2] += partition[1]
 			t[3] += partition[2]
 			t[4] += partition[0]
