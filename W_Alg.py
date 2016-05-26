2102bc0fa608453ca1a41d20a69f81c4b836ae0c
Qingyun Wu <qw2ky@virginia.edu>
2016-05-25 20:15:58 -0400

    Fix Learn W

--- W_Alg.py
+++ W_Alg.py
@@ -17,6 +17,7 @@ class WknowThetaStruct:
 
 		self.A = eta_ * np.identity(n = userNum*userNum)
 		self.b = np.zeros(userNum*userNum)
+		self.AInv = np.linalg.inv(self.A)
 		self.W = np.zeros(shape = (userNum, userNum))
 		self.CoTheta = np.dot(self.theta, self.W)
 
@@ -31,7 +32,7 @@ class WknowThetaStruct:
 		Xi_Matirx.T[userID] = articlePicked.featureVector
 		X = vectorize( np.dot(np.transpose(self.theta), Xi_Matirx))
 
-		#self.W = matrixize(np.dot(np.linalg.inv(self.A), self.b), self.userNum)
+		#self.W = matrixize(np.dot(self.AInv, self.b), self.userNum)
 		#fun = lambda x = np.identity(self.userNum): self.f1 + (1/2.0)*(np.dot( np.dot(articlePicked.featureVector, self.theta)  ,x.T[userID]) - click)**2 + (self.eta_/2.0)*np.matrix.trace(np.dot(np.transpose(x), x))
 		def fun(x):
 			x = np.asarray(x)
@@ -62,11 +63,12 @@ class WknowThetaStruct:
 		#self.W = normalize(self.W, axis=0, norm='l1')
 		self.A += np.outer(X, X)
 		self.b += click * X
-		self.W = matrixize(np.dot(np.linalg.inv(self.A), self.b), self.userNum)
+		self.AInv = np.linalg.inv(self.A)
+		self.W = matrixize(np.dot(self.AInv, self.b), self.userNum)
 
 		self.f1 +=(1/2.0)*(np.dot( np.dot(articlePicked.featureVector, self.theta)  ,self.W.T[userID]))**2
 		self.CoTheta = np.dot(self.theta, self.W)
-		self.CCA = np.dot(np.dot(self.BigTheta, np.linalg.inv(self.A)), np.transpose(self.BigTheta))
+		self.CCA = np.dot(np.dot(self.BigTheta, self.AInv), np.transpose(self.BigTheta))
 
 	def getProb(self, alpha, article, userID):
 		TempFeatureM = np.zeros(shape =(len(article.featureVector), self.userNum))
@@ -95,8 +97,13 @@ class WStruct:
 		# Basic stat in estimating W
 		self.W_A = eta_*np.identity(n = userNum*userNum)
 		self.W_b = np.zeros(userNum*userNum)
+		
+		self.W_AInv = np.linalg.inv(self.W_A)
+		#self.W = matrixize(np.dot(self.W_AInv, self.W_b), self.userNum)
 		#self.W =  np.zeros(shape = (userNum, userNum))
-		self.W = np.random.random((userNum, userNum))
+		self.W = np.identity(n = userNum)
+		#self.W = np.random.random((userNum, userNum))
+
 		for i in range(self.W.shape[0]):
 			self.W[i] /= sum(self.W[i]) 
 		
@@ -113,6 +120,7 @@ class WStruct:
 		T_X = vectorize(np.outer(articlePicked.featureVector, self.W.T[userID])) 
 		self.T_A += np.outer(T_X, T_X)	
 		self.T_b += click*T_X
+		self.T_AInv = np.linalg.inv(self.T_A)
 
 		Xi_Matirx = np.zeros(shape = (featureDimension, self.userNum))
 		Xi_Matirx.T[userID] = articlePicked.featureVector
@@ -120,9 +128,10 @@ class WStruct:
 		#print np.shape(W_X)
 		self.W_A += np.outer(W_X, W_X)
 		self.W_b += click * W_X
+		self.W_AInv = np.linalg.inv(self.W_A)
 
-		self.UserTheta = matrixize(np.dot(np.linalg.inv(self.T_A), self.T_b), len(articlePicked.featureVector)) 
-		self.W = matrixize(np.dot(np.linalg.inv(self.W_A), self.W_b), self.userNum)
+		self.UserTheta = matrixize(np.dot(self.T_AInv, self.T_b), len(articlePicked.featureVector)) 
+		self.W = matrixize(np.dot(self.W_AInv, self.W_b), self.userNum)
 		#self.W = normalize(self.W, axis=0, norm='l1')
 
 		#print 'A', self.W_A
@@ -135,11 +144,11 @@ class WStruct:
 
 		self.CoTheta = np.dot(self.UserTheta, self.W)
 		self.BigW = np.kron(np.transpose(self.W), np.identity(n=len(articlePicked.featureVector)))
-		self.CCA = np.dot(np.dot(self.BigW , np.linalg.inv(self.T_A)), np.transpose(self.BigW))
+		self.CCA = np.dot(np.dot(self.BigW , self.T_AInv), np.transpose(self.BigW))
 
 		self.BigTheta = np.kron(np.identity(n=self.userNum) , self.UserTheta)
 
-		self.W_CCA = np.dot(np.dot(self.BigTheta , np.linalg.inv(self.W_A)), np.transpose(self.BigTheta))
+		self.W_CCA = np.dot(np.dot(self.BigTheta , self.W_AInv), np.transpose(self.BigTheta))
 	
 	def getProb(self, alpha, article, userID):
 		TempFeatureM = np.zeros(shape =(len(article.featureVector), self.userNum))
@@ -170,6 +179,7 @@ class W_W0_Struct(WStruct):
 		T_X = vectorize(np.outer(articlePicked.featureVector, self.W.T[userID])) 
 		self.T_A += np.outer(T_X, T_X)	
 		self.T_b += click*T_X
+		self.T_AInv = np.linalg.inv(self.T_A)
 
 		Xi_Matirx = np.zeros(shape = (featureDimension, self.userNum))
 		Xi_Matirx.T[userID] = articlePicked.featureVector
@@ -177,9 +187,10 @@ class W_W0_Struct(WStruct):
 		#print np.shape(W_X)
 		self.W_A += np.outer(W_X, W_X)
 		self.W_b += click * W_X
+		self.W_AInv = np.linalg.inv(self.W_A)
 
-		self.UserTheta = matrixize(np.dot(np.linalg.inv(self.T_A), self.T_b), len(articlePicked.featureVector)) 
-		#self.W = matrixize(np.dot(np.linalg.inv(self.W_A), self.W_b), self.userNum)
+		self.UserTheta = matrixize(np.dot(self.T_AInv, self.T_b), len(articlePicked.featureVector)) 
+		#self.W = matrixize(np.dot(self.W_AInv, self.W_b), self.userNum)
 		#self.W = normalize(self.W, axis=0, norm='l1')
 
 		#print 'A', self.W_A
@@ -192,11 +203,11 @@ class W_W0_Struct(WStruct):
 
 		self.CoTheta = np.dot(self.UserTheta, self.W)
 		self.BigW = np.kron(np.transpose(self.W), np.identity(n=len(articlePicked.featureVector)))
-		self.CCA = np.dot(np.dot(self.BigW , np.linalg.inv(self.T_A)), np.transpose(self.BigW))
+		self.CCA = np.dot(np.dot(self.BigW , self.T_AInv), np.transpose(self.BigW))
 
 		self.BigTheta = np.kron(np.identity(n=self.userNum) , self.UserTheta)
 
-		self.W_CCA = np.dot(np.dot(self.BigTheta , np.linalg.inv(self.W_A)), np.transpose(self.BigTheta))
+		self.W_CCA = np.dot(np.dot(self.BigTheta , self.W_AInv ), np.transpose(self.BigTheta))
 
 
 class LearnW_W0_Struct(WStruct):
@@ -212,6 +223,7 @@ class LearnW_W0_Struct(WStruct):
 		T_X = vectorize(np.outer(articlePicked.featureVector, self.W.T[userID])) 
 		self.T_A += np.outer(T_X, T_X)	
 		self.T_b += click*T_X
+		self.T_AInv = np.linalg.inv(self.T_A)
 
 		Xi_Matirx = np.zeros(shape = (featureDimension, self.userNum))
 		Xi_Matirx.T[userID] = articlePicked.featureVector
@@ -219,9 +231,10 @@ class LearnW_W0_Struct(WStruct):
 		#print np.shape(W_X)
 		self.W_A += np.outer(W_X, W_X)
 		self.W_b += click * W_X
+		self.W_AInv = np.linalg.inv(self.W_A)
 
-		self.UserTheta = matrixize(np.dot(np.linalg.inv(self.T_A), self.T_b), len(articlePicked.featureVector)) 
-		self.W = matrixize(np.dot(np.linalg.inv(self.W_A), self.W_b), self.userNum)
+		self.UserTheta = matrixize(np.dot(self.T_AInv, self.T_b), len(articlePicked.featureVector)) 
+		self.W = matrixize(np.dot(self.W_AInv, self.W_b), self.userNum)
 		#self.W = normalize(self.W, axis=0, norm='l1')
 
 		#print 'A', self.W_A
@@ -234,11 +247,11 @@ class LearnW_W0_Struct(WStruct):
 
 		self.CoTheta = np.dot(self.UserTheta, self.W)
 		self.BigW = np.kron(np.transpose(self.W), np.identity(n=len(articlePicked.featureVector)))
-		self.CCA = np.dot(np.dot(self.BigW , np.linalg.inv(self.T_A)), np.transpose(self.BigW))
+		self.CCA = np.dot(np.dot(self.BigW , self.T_AInv), np.transpose(self.BigW))
 
 		self.BigTheta = np.kron(np.identity(n=self.userNum) , self.UserTheta)
 
-		self.W_CCA = np.dot(np.dot(self.BigTheta , np.linalg.inv(self.W_A)), np.transpose(self.BigTheta))
+		self.W_CCA = np.dot(np.dot(self.BigTheta , self.W_AInv), np.transpose(self.BigTheta))
 
 
 	
